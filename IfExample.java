35428a8644fc283db1dabbbc5bc17f3e7fe2e128
Rohit Gaikwad <70201567+Rohit7620@users.noreply.github.com>
2023-11-14 09:50:43 -0800

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,22 @@
+//if conditional statement
+package ConditionalStatament;
+import java.util.*;
+public class IfExample 
+{
+
+	public static void main(String[] args) 
+	{
+		int num1;
+		Scanner sc = new Scanner(System.in);//Accept input from user
+		System.out.println("Enter the value of num1 = ");
+		num1=sc.nextInt();
+		// checks numbers less than 10
+		if(num1<10)
+		{
+			System.out.println("Num1 is lesss than 10");
+		}
+		
+		
+	}
+
+}
